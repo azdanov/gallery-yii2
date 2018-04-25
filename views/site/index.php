1c8473cf472c6264274e37424bebad54668fe01d
Anton Zdanov <azdanov@protonmail.com>
2018-04-25 11:43:19 +0300

    feat: add simple profile page

--- views/site/index.php
+++ views/site/index.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 /* @var $this yii\web\View */
+/* @var $users app\models\User[]|array */
+
+use yii\helpers\Url;
 
 $this->title = Yii::$app->name;
 ?>
@@ -22,50 +25,12 @@ $this->title = Yii::$app->name;
 
     <div class="body-content">
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                    incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii
-                        Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                    incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum
-                        &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                    incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii
-                        Extensions &raquo;</a></p>
-            </div>
-        </div>
+        <?php foreach ($users as $user) : ?>
+            <a href="<?php echo Url::to(['/user/profile/view', 'id' => $user->id]); ?>">
+                <?php echo $user->username; ?>
+            </a>
+            <hr>
+        <?php endforeach; ?>
 
     </div>
 </div>
